efdc5fba75f393f493e1d042dfb5022a99c386a1
simonms22 <mejiass.simon@gmail.com>
2019-11-20 17:39:52 -0400

    Carga del navbar

--- react-hello/src/js/component/home.js
+++ react-hello/src/js/component/home.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import Navbar from "./navbar.js";
 import Jumbotron from "./jumbotron.js";
 import Cards from "./cards.js";
@@ -10,7 +9,7 @@ import Footer from "./footer.js";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
+		<div>
 			<Navbar />
 			<Jumbotron />
 			<Cards />
--- react-hello/src/js/component/navbar.js
+++ react-hello/src/js/component/navbar.js
@@ -1,6 +1,38 @@
 import React from "react";
 
 function Navbar() {
-	return <h1>Este es el Navbar</h1>;
+	return (
+		<nav className="navbar sticky-top navbar-dark bg-dark">
+			<a className="navbar-brand" href="#">
+				Start Bootstrap
+			</a>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNavAltMarkup"
+				aria-controls="navbarNavAltMarkup"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon" />
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div className="navbar-nav">
+					<a className="nav-item nav-link active" href="#">
+						Home <span className="sr-only">(current)</span>
+					</a>
+					<a className="nav-item nav-link" href="#">
+						About
+					</a>
+					<a className="nav-item nav-link" href="#">
+						Services
+					</a>
+					<a className="nav-item nav-link" href="#">
+						Contact
+					</a>
+				</div>
+			</div>
+		</nav>
+	);
 }
 export default Navbar;
