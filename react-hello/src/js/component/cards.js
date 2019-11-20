40bc60ce920f1300cd1844634a9b5c25a8c5a7d1
johneg02 <jonathanegarciasilveira@gmail.com>
2019-11-20 17:50:26 -0400

    cards

--- react-hello/src/js/component/cards.js
+++ react-hello/src/js/component/cards.js
@@ -1,45 +1,48 @@
 import React from "react";
 
 function Cards() {
-	return 
-	<div>
-		// primera imagen 
-		<div class="card" style="width: 18rem;">
-  			<img src="..." class="card-img-top" alt="...">
-  			<div class="card-body">
-    			<h5 class="card-title">Card title</h5>
-    			<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-    			<a href="#" class="btn btn-primary">Find Out More!!</a>
-  		</div>
+	return (
+		<div>
+			// primera imagen 
+			<div class="card" style="width: 18rem;">
+  				<img src="..." class="card-img-top" alt="..."/>
+  				<div class="card-body">
+    				<h5 class="card-title">Card title</h5>
+    				<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+    				<a href="#" class="btn btn-primary">Find Out More!!</a>
+				</div>	
+  			</div>
 
-		  // Segunda imagen 
-		<div class="card" style="width: 18rem;">
-  			<img src="..." class="card-img-top" alt="...">
-  			<div class="card-body">
-    			<h5 class="card-title">Card title</h5>
-    			<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-    			<a href="#" class="btn btn-primary">Find Out More!!</a>
-  		</div>  
+		  	// Segunda imagen 
+			<div class="card" style="width: 18rem;">
+  				<img src="..." class="card-img-top" alt="..."/>
+  				<div class="card-body">
+    				<h5 class="card-title">Card title</h5>
+    				<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+    				<a href="#" class="btn btn-primary">Find Out More!!</a>
+				</div>
+  			</div>  
 
-		  // Tercera imagen 
-		<div class="card" style="width: 18rem;">
-  			<img src="..." class="card-img-top" alt="...">
-  			<div class="card-body">
-    			<h5 class="card-title">Card title</h5>
-    			<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-    			<a href="#" class="btn btn-primary">Find Out More!!</a>
-  		</div>  
+		  	// Tercera imagen 
+			<div class="card" style="width: 18rem;">
+  				<img src="..." class="card-img-top" alt="..."/>
+  				<div class="card-body">
+    				<h5 class="card-title">Card title</h5>
+    				<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+    				<a href="#" class="btn btn-primary">Find Out More!!</a>
+				</div>
+  			</div>  
 
-		  // Cuarta imagen 
-		<div class="card" style="width: 18rem;">
-  			<img src="..." class="card-img-top" alt="...">
-  			<div class="card-body">
-    			<h5 class="card-title">Card title</h5>
-    			<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-    			<a href="#" class="btn btn-primary">Find Out More!!</a>
-  		</div>  
-</div>
-	
-	</div>;
+		  	// Cuarta imagen 
+			<div class="card" style="width: 18rem;">
+  				<img src="..." class="card-img-top" alt="..."/>
+  				<div class="card-body">
+    				<h5 class="card-title">Card title</h5>
+    				<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+    				<a href="#" class="btn btn-primary">Find Out More!!</a>
+				</div>
+  			</div>  
+		</div>
+	);
 }
 export default Cards;
--- react-hello/src/js/component/jumbotron.js
+++ react-hello/src/js/component/jumbotron.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 function Jumbotron() {
-	return <div class="jumbotron">
-	<h1 class="display-4">A Warm Welcome!</h1>
-	<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
-	<hr class="my-4">	
-	<a class="btn btn-primary btn-lg" href="#" role="button">Call to action!!!</a>
-  </div>;
+	return (
+		<div class="jumbotron">
+			<h1 class="display-4">A Warm Welcome!</h1>
+			<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
+			<hr class="my-4">	
+			<a class="btn btn-primary btn-lg" href="#" role="button">Call to action!!!</a>
+  		</div>
+  	);
 }
 export default Jumbotron;
